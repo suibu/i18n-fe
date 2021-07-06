70a4a31927e7d22d54187fc1cad97a923f16933e
suibu <walk-liuyan@outlook.com>
2021-07-06 16:40:55 +0800

    A：粗糙版本的lerna使用

---
+++ packages/core/bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const core = require('../lib')
+console.log('test!!')
+core()
--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = core;
+const utils = require('@suibu-i18n-fe/utils')
 
 function core() {
-    // TODO
+    utils()
 }
