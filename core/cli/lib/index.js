bf69c2a69253fa231a7c393aade0454578ddef77
suibu <walk-liuyan@outlook.com>
2021-07-22 23:54:56 +0800

    A: 检查node版本

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,15 +1,34 @@
 'use strict';
 
 module.exports = core;
+const semver = require('semver')
+const colors = require('colors/safe')
 const pkg = require('../package.json');
 
 const utils = require('@i18n-fe/utils')
 const log = require('@i18n-fe/log')
 
+const constant = require('./const')
+
 function core() {
-    checkPkgVersion()
+    try {
+        checkPkgVersion()
+        checkNodeVersion()
+    } catch(e) {
+        log.error(e.message)
+    }
+}
+function checkNodeVersion() {
+// 1、当前node version
+    const curVersion = process.version;
+// 2、比对 当前版本与最低版本
+    const lowestVersion = constant.LOWEST_NODE_VERSION
+    if (!semver.gte(curVersion, lowestVersion)) {
+        throw new Error(colors.red(`need more than node v14 +`))
+    }
 }
 
 function checkPkgVersion() {
     log.success('version', pkg.version)
 }
+
