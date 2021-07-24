509ed37569893aabb2f55c78aa07971e1e1abd7a
suibu <walk-liuyan@outlook.com>
2021-07-25 00:07:23 +0800

    A: 检查root启动

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -14,6 +14,7 @@ function core() {
     try {
         checkPkgVersion()
         checkNodeVersion()
+        checkRoot()
     } catch(e) {
         log.error(e.message)
     }
@@ -32,3 +33,12 @@ function checkPkgVersion() {
     log.success('version', pkg.version)
 }
 
+function checkRoot() {
+    // 当前用户OS
+    const curUserAuthCode = process.geteuid();
+    const rootCheck = require('root-check');
+    // 实现原理是：root用户,process.getuid() === 0。普通用户，为501。若检测到是root用户，就降级操作process.seteuid()
+    // 为什么要做这步操作：当root用户操作的一些文件后，普通用户在进来操作会有权限问题
+    rootCheck()
+    console.log(process.geteuid())
+}
